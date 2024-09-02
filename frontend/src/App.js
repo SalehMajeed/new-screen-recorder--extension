bd6248a15459caccc549ba2d8926e2b258d0c3ef
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2024-09-02 15:55:38 +0530

    convert ui from html to react components

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import VideoUI from './Components/VideoUI';
+
+function App() {
+  return (
+    <div className="App">
+     <VideoUI />
+    </div>
+  );
+}
+
+export default App;
